999ccb4274709e7082f40c3a3ceead48997c5efd
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2024-09-29 22:01:38 +0800

    Fixed undefined drawUserInterface due to being in a different scope

--- js/userInterface.js
+++ js/userInterface.js
@@ -190,156 +190,156 @@ function checkButtonHandling() {
       return false;
     }
   }
+}
 
-  function drawUserInterface() {
-    if (startDelayTimer) {
-      buttonDelayTicks--;
-      if (buttonDelayTicks <= 0) {
-        buttonDelayTimer = true;
-        startDelayTimer = false;
-      }
+function drawUserInterface() {
+  if (startDelayTimer) {
+    buttonDelayTicks--;
+    if (buttonDelayTicks <= 0) {
+      buttonDelayTimer = true;
+      startDelayTimer = false;
+    }
+  }
+
+  if (peasantSelected || warriorSelected) { //will change this shortly to a different requirement
+    drawBitmapAtLocation(peasantPic, 0, 60, 15, 15, 100, 10);
+    drawBitmapAtLocation(warriorPic, 0, 60, 20, 20, 160, 8);
+    colorText(" = " + playerUnits.length, 120, 22, "White", "14px Arial");
+    colorText(" = " + playerUnits.length, 180, 22, "White", "14px Arial");
+  }
+
+  if (peasantSelected && !constructionButtonSelected) {
+    drawBitmapAtLocation(peasantProfilePic, 0, 120, pictureWidth, pictureHeight, 10, 36);
+    colorText("PEASANT", 9, 20, "Yellow", "14px Arial");
+    colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
+    //construction
+    if (constructionButtonHovering && mouseClicked) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 60, pictureWidth, pictureHeight, constructionX, constructionY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 60, pictureWidth, pictureHeight, constructionX, constructionY);
+    }
+    //lumber
+    if (lumberButtonHovering && mouseClicked) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 60, pictureWidth, pictureHeight, lumberX, lumberY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 60, pictureWidth, pictureHeight, lumberX, lumberY);
+    }
+    //construction
+    if (constructionButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 360, pictureWidth, pictureHeight, constructionX, constructionY);
+      colorText("BUILD", 18, constructionY + pictureHeight + 15, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 360, pictureWidth, pictureHeight, constructionX, constructionY);
+      colorText("BUILD", 18, constructionY + pictureHeight + 15, "White", "14px Arial");
+    }
+    if (lumberButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, lumberX, lumberY);
+      colorText("GATHER", 9, lumberY + pictureHeight + 15, "Yellow", "14px Arial");
+      colorText("LUMBER", 9, lumberY + pictureHeight + 29, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, lumberX, lumberY);
+      colorText("GATHER", 9, lumberY + pictureHeight + 15, "White", "14px Arial");
+      colorText("LUMBER", 9, lumberY + pictureHeight + 29, "White", "14px Arial");
+    }
+    if (lumberButtonHovering && mouseClicked) { //text
+      colorText("GATHER", 9, lumberY + pictureHeight + 15, "Green", "14px Arial");
+      colorText("LUMBER", 9, lumberY + pictureHeight + 29, "Green", "14px Arial");
+    }
+    //attacking
+    if (attackButtonHovering && mouseClicked) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 180, pictureWidth, pictureHeight, attackX, attackY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 180, pictureWidth, pictureHeight, attackX, attackY);
+    }
+    if (attackButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, attackX, attackY);
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, attackX, attackY);
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "White", "14px Arial");
+    }
+    if (attackButtonHovering && mouseClicked) { //text
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "Green", "14px Arial");
+    }
+    //gold mining
+    if (goldButtonHovering && mouseClicked) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 240, pictureWidth, pictureHeight, goldX, goldY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 240, pictureWidth, pictureHeight, goldX, goldY);
+    }
+    if (goldButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, goldX, goldY);
+      colorText("MINE", 19, goldY + pictureHeight + 15, "Yellow", "14px Arial");
+      colorText("GOLD", 18, goldY + pictureHeight + 29, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, goldX, goldY);
+      colorText("MINE", 19, goldY + pictureHeight + 15, "White", "14px Arial");
+      colorText("GOLD", 18, goldY + pictureHeight + 29, "White", "14px Arial");
+    }
+    if (goldButtonHovering && mouseClicked) { //text
+      colorText("MINE", 19, goldY + pictureHeight + 15, "Green", "14px Arial");
+      colorText("GOLD", 18, goldY + pictureHeight + 29, "Green", "14px Arial");
+    }
+    //farming
+    if (farmButtonHovering && mouseClicked) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 300, pictureWidth, pictureHeight, farmX, farmY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 300, pictureWidth, pictureHeight, farmX, farmY);
+    }
+    if (farmButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, farmX, farmY);
+      colorText("FARM", 19, farmY + pictureHeight + 15, "Yellow", "14px Arial");
+      colorText("FOOD", 18, farmY + pictureHeight + 29, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, farmX, farmY);
+      colorText("FARM", 19, farmY + pictureHeight + 15, "White", "14px Arial");
+      colorText("FOOD", 18, farmY + pictureHeight + 29, "White", "14px Arial");
+    }
+    if (farmButtonHovering && mouseClicked) { //text
+      colorText("FARM", 19, farmY + pictureHeight + 15, "Green", "14px Arial");
+      colorText("FOOD", 18, farmY + pictureHeight + 29, "Green", "14px Arial");
     }
 
-    if (peasantSelected || warriorSelected) { //will change this shortly to a different requirement
-      drawBitmapAtLocation(peasantPic, 0, 60, 15, 15, 100, 10);
-      drawBitmapAtLocation(warriorPic, 0, 60, 20, 20, 160, 8);
-      colorText(" = " + playerUnits.length, 120, 22, "White", "14px Arial");
-      colorText(" = " + playerUnits.length, 180, 22, "White", "14px Arial");
+  } else if (constructionButtonSelected) { //// Construction Screen
+    drawBitmapAtLocation(peasantProfilePic, 0, 120, pictureWidth, pictureHeight, 10, 36);
+    colorText("PEASANT", 9, 20, "Yellow", "14px Arial");
+    colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
+
+    if (buildWallHoovering) {
+      drawBitmapAtLocation(framePic, 60, 420, pictureWidth, pictureHeight, wallX, wallY);
+      colorText("WALL", 21, wallY + pictureHeight + 15, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 420, pictureWidth, pictureHeight, wallX, wallY);
+      colorText("WALL", 21, wallY + pictureHeight + 15, "White", "14px Arial");
+    }
+    if (farmBuildHoovering) {
+      drawBitmapAtLocation(framePic, 60, 480, pictureWidth, pictureHeight, farmBuildX, farmBuildY);
+      colorText("FARM", 21, farmBuildY + pictureHeight + 15, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 480, pictureWidth, pictureHeight, farmBuildX, farmBuildY);
+      colorText("FARM", 21, farmBuildY + pictureHeight + 15, "White", "14px Arial");
     }
 
-    if (peasantSelected && !constructionButtonSelected) {
-      drawBitmapAtLocation(peasantProfilePic, 0, 120, pictureWidth, pictureHeight, 10, 36);
-      colorText("PEASANT", 9, 20, "Yellow", "14px Arial");
-      colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
-      //construction
-      if (constructionButtonHovering && mouseClicked) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 60, pictureWidth, pictureHeight, constructionX, constructionY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 60, pictureWidth, pictureHeight, constructionX, constructionY);
-      }
-      //lumber
-      if (lumberButtonHovering && mouseClicked) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 60, pictureWidth, pictureHeight, lumberX, lumberY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 60, pictureWidth, pictureHeight, lumberX, lumberY);
-      }
-      //construction
-      if (constructionButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 360, pictureWidth, pictureHeight, constructionX, constructionY);
-        colorText("BUILD", 18, constructionY + pictureHeight + 15, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 360, pictureWidth, pictureHeight, constructionX, constructionY);
-        colorText("BUILD", 18, constructionY + pictureHeight + 15, "White", "14px Arial");
-      }
-      if (lumberButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, lumberX, lumberY);
-        colorText("GATHER", 9, lumberY + pictureHeight + 15, "Yellow", "14px Arial");
-        colorText("LUMBER", 9, lumberY + pictureHeight + 29, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, lumberX, lumberY);
-        colorText("GATHER", 9, lumberY + pictureHeight + 15, "White", "14px Arial");
-        colorText("LUMBER", 9, lumberY + pictureHeight + 29, "White", "14px Arial");
-      }
-      if (lumberButtonHovering && mouseClicked) { //text
-        colorText("GATHER", 9, lumberY + pictureHeight + 15, "Green", "14px Arial");
-        colorText("LUMBER", 9, lumberY + pictureHeight + 29, "Green", "14px Arial");
-      }
-      //attacking
-      if (attackButtonHovering && mouseClicked) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 180, pictureWidth, pictureHeight, attackX, attackY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 180, pictureWidth, pictureHeight, attackX, attackY);
-      }
-      if (attackButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, attackX, attackY);
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, attackX, attackY);
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "White", "14px Arial");
-      }
-      if (attackButtonHovering && mouseClicked) { //text
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "Green", "14px Arial");
-      }
-      //gold mining
-      if (goldButtonHovering && mouseClicked) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 240, pictureWidth, pictureHeight, goldX, goldY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 240, pictureWidth, pictureHeight, goldX, goldY);
-      }
-      if (goldButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, goldX, goldY);
-        colorText("MINE", 19, goldY + pictureHeight + 15, "Yellow", "14px Arial");
-        colorText("GOLD", 18, goldY + pictureHeight + 29, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, goldX, goldY);
-        colorText("MINE", 19, goldY + pictureHeight + 15, "White", "14px Arial");
-        colorText("GOLD", 18, goldY + pictureHeight + 29, "White", "14px Arial");
-      }
-      if (goldButtonHovering && mouseClicked) { //text
-        colorText("MINE", 19, goldY + pictureHeight + 15, "Green", "14px Arial");
-        colorText("GOLD", 18, goldY + pictureHeight + 29, "Green", "14px Arial");
-      }
-      //farming
-      if (farmButtonHovering && mouseClicked) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 300, pictureWidth, pictureHeight, farmX, farmY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 300, pictureWidth, pictureHeight, farmX, farmY);
-      }
-      if (farmButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, farmX, farmY);
-        colorText("FARM", 19, farmY + pictureHeight + 15, "Yellow", "14px Arial");
-        colorText("FOOD", 18, farmY + pictureHeight + 29, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, farmX, farmY);
-        colorText("FARM", 19, farmY + pictureHeight + 15, "White", "14px Arial");
-        colorText("FOOD", 18, farmY + pictureHeight + 29, "White", "14px Arial");
-      }
-      if (farmButtonHovering && mouseClicked) { //text
-        colorText("FARM", 19, farmY + pictureHeight + 15, "Green", "14px Arial");
-        colorText("FOOD", 18, farmY + pictureHeight + 29, "Green", "14px Arial");
-      }
+  } else if (warriorSelected) {
+    drawBitmapAtLocation(peasantProfilePic, pictureWidth * 2, 120, pictureWidth, pictureHeight, 10, 36); //warrior
+    colorText("WARRIOR", 9, 20, "Yellow", "14px Arial");
+    colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
 
-    } else if (constructionButtonSelected) { //// Construction Screen
-      drawBitmapAtLocation(peasantProfilePic, 0, 120, pictureWidth, pictureHeight, 10, 36);
-      colorText("PEASANT", 9, 20, "Yellow", "14px Arial");
-      colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
-
-      if (buildWallHoovering) {
-        drawBitmapAtLocation(framePic, 60, 420, pictureWidth, pictureHeight, wallX, wallY);
-        colorText("WALL", 21, wallY + pictureHeight + 15, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 420, pictureWidth, pictureHeight, wallX, wallY);
-        colorText("WALL", 21, wallY + pictureHeight + 15, "White", "14px Arial");
-      }
-      if (farmBuildHoovering) {
-        drawBitmapAtLocation(framePic, 60, 480, pictureWidth, pictureHeight, farmBuildX, farmBuildY);
-        colorText("FARM", 21, farmBuildY + pictureHeight + 15, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 480, pictureWidth, pictureHeight, farmBuildX, farmBuildY);
-        colorText("FARM", 21, farmBuildY + pictureHeight + 15, "White", "14px Arial");
-      }
-
-    } else if (warriorSelected) {
-      drawBitmapAtLocation(peasantProfilePic, pictureWidth * 2, 120, pictureWidth, pictureHeight, 10, 36); //warrior
-      colorText("WARRIOR", 9, 20, "Yellow", "14px Arial");
-      colorText("OPTIONS", 9, 116, "Yellow", "14px Arial");
-
-      //attacking
-      if (attackButtonHovering && mouseClicked && buttonDelayTimer) { //picture
-        drawBitmapAtLocation(lumberPic, 60, 180, pictureWidth, pictureHeight, attackX, attackY);
-      } else {
-        drawBitmapAtLocation(lumberPic, 0, 180, pictureWidth, pictureHeight, attackX, attackY);
-      }
-      if (attackButtonHovering) { //frame
-        drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, attackX, attackY);
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "Yellow", "14px Arial");
-      } else {
-        drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, attackX, attackY);
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "White", "14px Arial");
-      }
-      if (attackButtonHovering && mouseClicked) { //text
-        colorText("ATTACK", 11, attackY + pictureHeight + 15, "Green", "14px Arial");
-      }
+    //attacking
+    if (attackButtonHovering && mouseClicked && buttonDelayTimer) { //picture
+      drawBitmapAtLocation(lumberPic, 60, 180, pictureWidth, pictureHeight, attackX, attackY);
+    } else {
+      drawBitmapAtLocation(lumberPic, 0, 180, pictureWidth, pictureHeight, attackX, attackY);
+    }
+    if (attackButtonHovering) { //frame
+      drawBitmapAtLocation(framePic, 60, 0, pictureWidth, pictureHeight, attackX, attackY);
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "Yellow", "14px Arial");
+    } else {
+      drawBitmapAtLocation(framePic, 0, 0, pictureWidth, pictureHeight, attackX, attackY);
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "White", "14px Arial");
+    }
+    if (attackButtonHovering && mouseClicked) { //text
+      colorText("ATTACK", 11, attackY + pictureHeight + 15, "Green", "14px Arial");
     }
   }
 }
