e2b91db6bf93a5db063d32b70277c444f8a4fbf9
Vince McKeown <vmckeown79@gmail.com>
2024-10-09 22:30:13 -0400

    Refactored Building Class

--- js/Buildings.js
+++ js/Buildings.js
@@ -7,76 +7,74 @@ function buildingClass(building) {
         // ensure the unit is not standing on water etc
         let validLocation = false;
         while (!validLocation) {
-            this.x = Math.random()*WORLD_SIZE_PIXELS_W;
-            this.y = Math.random()*WORLD_SIZE_PIXELS_H;
-            let index = colRowToIndex (this.x,this.y);
-            validLocation = presetUnwalkableTiles.indexOf(index)==-1;
+            this.x = Math.random() * WORLD_SIZE_PIXELS_W;
+            this.y = Math.random() * WORLD_SIZE_PIXELS_H;
+            let index = colRowToIndex(this.x, this.y);
+            validLocation = presetUnwalkableTiles.indexOf(index) === -1;
         }
+    
         this.width = 100;
         this.height = 100;
         this.pic = goblinHQPic;
         this.lumber = 0;
-        
-        if(this.type == "players hq") {
-            this.pic = humanHQPic;
-            this.sX = 0;
-            this.sY = 0;
-            this.unitColor = 'Red';
-            this.health = 50;
-            this.minimapDrawPriority = 1;
-        } else if (this.type == "goblin hq") {
+        this.sX = 0;
+        this.sY = 0;
+        this.health = 50;
+        this.unitColor = 'White';
+        this.minimapDrawPriority = 1;
+    
+        const adjustForComputer = () => {
             this.x = WORLD_SIZE_PIXELS_W - this.x - 900;
             this.y = WORLD_SIZE_PIXELS_H - this.y - 900;
-            this.sX = 0;
-            this.sY = 0;
-            this.unitColor = 'White';
-            this.pic = goblinHQPic;
-            this.health = 50;
-            this.minimapDrawPriority = 1;
-        } else if (this.type == "orc barrack") {
-            this.x = WORLD_SIZE_PIXELS_W - this.x - 900;
-            this.y = WORLD_SIZE_PIXELS_H - this.y - 900;
-            this.sX = 0;
-            this.sY = 0;
-            this.unitColor = 'White';
-            this.pic = orcBarrackPic;
-            this.health = 50;
-            this.minimapDrawPriority = 2;
-        } else if (this.type == "peasant farm") {
-            this.x = mouseX;
-            this.y = mouseY;
-            this.sX = 0;
-            this.sY = 50;
-            this.height = 75;
-            this.unitColor = 'White';
-            this.pic = orcFarmPic;
-            this.health = 10;
-            this.minimapDrawPriority = 2;
-            console.log("Peasant Farm created")
-        } else if (this.type == "orc farm") {
-            this.x = WORLD_SIZE_PIXELS_W - this.x - 900;
-            this.y = WORLD_SIZE_PIXELS_H - this.y - 900;
-            this.sX = 0;
-            this.sY = 0;
-            this.height = 50;
-            this.unitColor = 'White';
-            this.pic = orcFarmPic;
-            this.health = 10;
-            this.minimapDrawPriority = 2;
-        } else if (this.type == "wall"){
-            this.x = mouseX;
-            this.y = mouseY;
-            this.pic = wallPic;
-            this.height = 32;
-            this.width = 32;
-            this.sX = 32*3;
-            this.sY = 0;
-            this.unitColor = 'Red';
-            this.health = 50;
-            this.minimapDrawPriority = 3;
-            this.snapNearestGridCenter();
-        } 
-    }
+        };
+    
+        switch (this.type) {
+            case "players hq":
+                this.pic = humanHQPic;
+                this.unitColor = 'Red';
+                break;
+            case "wall":
+                this.x = mouseX;
+                this.y = mouseY;
+                this.width = 32;
+                this.height = 32;
+                this.pic = wallPic;
+                this.sX = 32 * 3;
+                this.unitColor = 'Red';
+                this.health = 50;
+                this.minimapDrawPriority = 3;
+                this.snapNearestGridCenter();
+                break;
+            case "peasant farm":
+                this.x = mouseX;
+                this.y = mouseY;
+                this.height = 60;
+                this.sy = 50;
+                this.pic = orcFarmPic;
+                this.health = 10;
+                this.minimapDrawPriority = 2;
+                break;
+            //Computer buildings
+            case "goblin hq":
+                adjustForComputer();
+                this.pic = goblinHQPic;
+                break;
+            case "orc barrack":
+                adjustForComputer();
+                this.pic = orcBarrackPic;
+                this.minimapDrawPriority = 2;
+                break;
+            case "orc farm":
+                adjustForComputer();
+                this.height = 50;
+                this.pic = orcFarmPic;
+                this.health = 10;
+                this.minimapDrawPriority = 2;
+                break;
+            default:
+                console.warn(`Unknown type: ${this.type}`);
+        }
+    };
 
     this.snapNearestGridCenter = function (){
         this.x = Math.floor(this.x/GRID_WIDTH)*GRID_WIDTH + GRID_WIDTH/2;
